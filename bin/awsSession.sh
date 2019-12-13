55c1c1fbdee8fda1b5857f2e8ed8e6620a831128
Dave Knappenberger <kb7ky0@gmail.com>
2019-12-12 22:12:00 -0700

    fix hanging comma on awsSession.sh

--- bin/awsSession.sh
+++ bin/awsSession.sh
@@ -35,9 +35,9 @@ chmod 600 $AWSOUT/credentials
 # dtk account
 echo Account dtk
 aws --profile default sts get-session-token --serial-number 'arn:aws:iam::100311576175:mfa/dtk' --token-code $1 > $TOKENFILE
-DTK_AKID=`grep AccessKeyId $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
-DTK_SAK=`grep SecretAccessKey $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
-DTK_ST=`grep SessionToken $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
+DTK_AKID=`grep AccessKeyId $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
+DTK_SAK=`grep SecretAccessKey $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
+DTK_ST=`grep SessionToken $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
 echo -e "\n " >> $AWSOUT/credentials
 echo '[dtk]' >> $AWSOUT/credentials
 echo "aws_access_key_id = $DTK_AKID" >> $AWSOUT/credentials
@@ -47,16 +47,16 @@ echo "aws_session_token = $DTK_ST" >> $AWSOUT/credentials
 # aft account
 echo Account aft
 aws --profile ff sts get-session-token --serial-number 'arn:aws:iam::357369783546:mfa/aft-dtk' --token-code $2 > $TOKENFILE
-AFT_AKID=`grep AccessKeyId $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
-AFT_SAK=`grep SecretAccessKey $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
-AFT_ST=`grep SessionToken $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | sed s/,$// | xargs`
+AFT_AKID=`grep AccessKeyId $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
+AFT_SAK=`grep SecretAccessKey $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
+AFT_ST=`grep SessionToken $TOKENFILE | cut -f2 -d ':' | sed s/\"//g | tr -d ',' | xargs --`
 echo -e "\n " >> $AWSOUT/credentials
 echo '[aft]' >> $AWSOUT/credentials
 echo "aws_access_key_id = $AFT_AKID" >> $AWSOUT/credentials
 echo "aws_secret_access_key = $AFT_SAK" >> $AWSOUT/credentials
 echo "aws_session_token = $AFT_ST" >> $AWSOUT/credentials
 
-rm -f $TOKENFILE
+# rm -f $TOKENFILE
 
 
 
